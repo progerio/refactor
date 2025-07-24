0fe9711f2c0ffc879f7853f56a98600f89ed07a3
Paulo Rogerio <paulo@intranet>
2025-07-24 15:46:08 -0300

    Refactor statement function to enhance clarity by restructuring data handling and improving performance calculations

--- src/cap1/statement.js
+++ src/cap1/statement.js
@@ -1,51 +1,26 @@
 
 export default function statment(invoice, plays) {
-    return renderPlainText(invoice, plays);
-}   
-function renderPlainText(invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
-        result += ` ${playFor(perf).name}:${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
+    const statmentData = {};
+    statmentData.customer = invoice.customer;
+    statmentData.performances = invoice.performances.map(enrichPerformance);
+    statmentData.totalAmount = totalAmount(statmentData );
+    statmentData.totalVolumeCredits = totalVolumeCredits(statmentData);
+    return renderPlainText(statmentData, plays);
 
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2
-            }).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
+
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy": result = 40000;
                 if (aPerformance.audience > 30) {
                     result += 1000 * (aPerformance.audience - 30);
@@ -58,8 +33,42 @@ function renderPlainText(invoice, plays) {
                 }
                 result += 300 * aPerformance.audience;
                 break;
-            default: throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+            default: throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+    
+    function totalAmount(data ) {
+        return data.performances.reduce((total, perf) => total + perf.amount, 0);
+    }
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, perf) => total + perf.volumeCredits, 0);
+    }
+}   
+
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}:${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+
+    
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
     }
 } 
