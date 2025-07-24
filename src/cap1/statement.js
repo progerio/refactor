2985170ad1642ea984dfe5283dab0cd1bad5aac4
Paulo Rogerio <paulo@intranet>
2025-07-24 13:27:57 -0300

    Refactor statement function to move volume credits calculation after performance processing for clarity

--- src/cap1/statement.js
+++ src/cap1/statement.js
@@ -3,13 +3,16 @@ import  plays  from "./plays.json";
 
 export default function statment(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
+    
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-        // exibe a linha para esta requisição
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+    
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
     
     result += `Amount owed is ${usd(totalAmount)}\n`;
