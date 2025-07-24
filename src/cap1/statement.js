d0c7bf051b8cb274689cad99c20fb474c1209465
Paulo Rogerio <paulo@intranet>
2025-07-24 14:59:32 -0300

    Fix typo in function name for statement export

--- src/cap1/statement.js
+++ src/cap1/statement.js
@@ -1,5 +1,8 @@
 
-export default function statement(invoice, plays) {
+export default function statment(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}   
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}:${usd(amountFor(perf))} (${perf.audience} seats)\n`;
