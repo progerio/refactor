cf90178e30e6ccc123a0cb475ddb3bc9678b2746
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2025-07-29 19:28:07 -0300

    Refactor createStatementData to use PerformanceCalculator for amount and volume credits calculations

--- src/cap1/statement.js
+++ src/cap1/statement.js
@@ -30,6 +30,7 @@ function htmlStatement(invoice, plays) {
 }
 
 function renderHtml(data) {
+    
     let result = `<h1>Statement for ${data.customer}</h1>\n`;
     result += "<table>\n";
     result += "<tr><th>play</th><th>seats</th><th>amount</th></tr>\n";
